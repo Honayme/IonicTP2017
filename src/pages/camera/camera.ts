dbe12032c22ac9a618b6a0e28299b2753ee5a367
Thomas <honayme@gmail.com>
2017-10-26 15:07:20 +0200

    add camera method

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,21 +1,25 @@
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+  public base64Image: string;
+
 constructor(private camera: Camera) { }
 
-...
 
-
-const options: CameraOptions = {
-  quality: 100,
-  destinationType: this.camera.DestinationType.DATA_URL,
-  encodingType: this.camera.EncodingType.JPEG,
-  mediaType: this.camera.MediaType.PICTURE
+takePicture(){
+  Camera.getPicture({
+    destinationType: Camera.DestinationType.DATA_URL,
+    targetWidth: 1000,
+    targetHeight: 1000
+  }).then((imageData) => {
+    // imageData is a base64 encoded string
+    this.base64Image = "data:image/jpeg;base64," + imageData;
+  }, (err) => {
+    console.log(err);
+  });
 }
-
-this.camera.getPicture(options).then((imageData) => {
-  // imageData is either a base64 encoded string or a file URI
-  // If it's base64:
-  let base64Image = 'data:image/jpeg;base64,' + imageData;
-}, (err) => {
-  // Handle error
-});
