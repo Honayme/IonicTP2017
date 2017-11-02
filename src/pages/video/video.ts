a7c5b8805f71f051bd8a1501ab71c99e0a26c90c
Thomas <honayme@gmail.com>
2017-11-02 14:46:40 +0100

    correctif LocalNotifications

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 // import {CaptureImageOptions, CaptureVideoOptions} from "@ionic-native/media-capture";
 import {MediaCapture, MediaFile, CaptureError, CaptureVideoOptions} from '@ionic-native/media-capture';
+import {LocalNotifications} from "@ionic-native/local-notifications";
 /**
  * Generated class for the VideoPage page.
  *
@@ -14,29 +15,34 @@ import {MediaCapture, MediaFile, CaptureError, CaptureVideoOptions} from '@ionic
   selector: 'page-video',
   templateUrl: 'video.html',
 })
+
 export class VideoPage {
+  public path: string;
   public message : string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private mediaCapture: MediaCapture) {
-  }
+  public options : {limit: 1}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VideoPage');
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private mediaCapture: MediaCapture,
+              private localNotifications: LocalNotifications) {
   }
-
-  public options: CaptureVideoOptions = {};
 
   takeVideo() {
     this.mediaCapture.captureVideo(this.options)
       .then(
-        (data: MediaFile[]) => console.log(data),
-        (err: CaptureError) => console.error(err)
-      ), (err) => {
-      console.log(err);
-      this.message = "camera";
+        (data: MediaFile[]) => {
+          this.path = data[0].fullPath;
 
-    }
-
-
+          this.localNotifications.schedule({
+            text: `Vidéo enregistré dans ${this.path}`
+          })
+        },
+        (err: CaptureError) => {
+          this.localNotifications.schedule({
+            text: `Warning ${err}`
+          })
+        }
+      )
   }
 
 
